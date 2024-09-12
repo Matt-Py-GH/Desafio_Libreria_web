b2a4883c2b9498391fda7a57f16b2a6369a8464c
Mateo <mateldraka@gmail.com>
2024-09-12 18:32:35 -0300

    pusheo antes de irme a baar

--- clases/Program.cs
+++ clases/Program.cs
@@ -6,10 +6,12 @@ namespace Libreria_web {
     {
         static void Main()
         {
-            List<Libro> Pedido = new List<Libro>();
+            
             Usuario usuario = new Usuario();
-            
-            
+            Libreria libreria = new Libreria();
+            TarjetaDeCredito tarjeta = new TarjetaDeCredito();
+
+
             bool exito = false;
             while (!exito)
             {
@@ -32,79 +34,105 @@ namespace Libreria_web {
             {
                 Console.WriteLine($"{libro.Nombre}, {libro.Precio}, {libro.TipoLibro}, {libro.Cantidad}, {libro.CodigoLibro}\n");  
             }
-            
-            
-        }
-
-        public void ComprarLibros()
-        {
-            Libreria libreria = new Libreria();
-            TarjetaDeCredito tarjeta = new TarjetaDeCredito();
-            double totalCompra = 0;
-            bool comprado = false;
-            while (!comprado)
+            Console.WriteLine("1.Agregar libros al carrito\n2.Remover libros\n3.Ver carrito\n4.Salir");
+            string opcionMenu = "0";
+            while (opcionMenu != "3")
             {
-            Console.WriteLine("Seleccione los libros que desee comprar (escriba 00 para salir)");
-            int codigoElegido = int.Parse(Console.ReadLine());
-            bool encontrado = false;
-            foreach (var libro in libreria.Lista)
-            {
-                if (codigoElegido == libro.CodigoLibro)
+                switch (opcionMenu)
                 {
-                    encontrado = true;
-                    if (encontrado == true)
-                    {
-                        Console.WriteLine("Elija cuantos quiere:");
-                        int cantidadElegida = int.Parse(Console.ReadLine());
-                        if (cantidadElegida <= libro.Cantidad)
-                        {
-                            Console.WriteLine("Introduzca num tarjeta, CVC");
-                            string num = Console.ReadLine();
-                            string CVC = Console.ReadLine();
+                    case "1":
+                        Console.WriteLine("Agregar libros al carrito");
+                        ComprarLibros();
+                        break;
 
-                            if (num == tarjeta.NumeroTarjeta.ToString() && CVC == tarjeta.CodigoSeguridad.ToString())
+                    case "2":
+                        Console.WriteLine("Eliminar libros del carrito");
+                        break;
+                    case "3":
+                        Console.WriteLine("Saliendo del sistema...");
+                        break;
+                    case "4":
+                        Console.WriteLine("Saliendo del sistema...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opcion ingresada no valida");
+                        break;
+
+                }//Switch
+            }//While menu
+            void ComprarLibros()
+            {
+                List<Libro> Pedido = new List<Libro>();
+                Libreria libreria = new Libreria();
+                TarjetaDeCredito tarjeta = new TarjetaDeCredito();
+                double totalCompra = 0;
+                bool comprado = false;
+                while (!comprado)
+                {
+                    Console.WriteLine("Seleccione los libros que desee comprar (escriba 00 para salir)");
+                    int codigoElegido = int.Parse(Console.ReadLine());
+                    bool encontrado = false;
+                    foreach (var libro in libreria.Lista)
+                    {
+                        if (codigoElegido == libro.CodigoLibro)
+                        {
+                            encontrado = true;
+                            if (encontrado == true)
                             {
-                                if (tarjeta.Saldo >= cantidadElegida * libro.Precio)
+                                Console.WriteLine("Elija cuantos quiere:");
+                                int cantidadElegida = int.Parse(Console.ReadLine());
+                                if (cantidadElegida <= libro.Cantidad)
                                 {
-                                    for (int i = 0; i < cantidadElegida; i++)
+                                    Console.WriteLine("Introduzca num tarjeta, CVC");
+                                    string num = Console.ReadLine();
+                                    string CVC = Console.ReadLine();
+
+                                    if (num == tarjeta.NumeroTarjeta.ToString() && CVC == tarjeta.CodigoSeguridad.ToString())
                                     {
-                                        Pedido.Add(libro);
+                                        if (tarjeta.Saldo >= cantidadElegida * libro.Precio)
+                                        {
+                                            for (int i = 0; i < cantidadElegida; i++)
+                                            {
+                                                Pedido.Add(libro);
+                                            }
+                                            totalCompra += libro.Precio * cantidadElegida;
+                                            tarjeta.Saldo -= cantidadElegida * libro.Precio;
+                                            libro.Cantidad -= cantidadElegida;
+                                            Console.WriteLine($"Su total es: {totalCompra}");
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Saldo insuficiente...");
+                                        }
                                     }
-                                    totalCompra += libro.Precio * cantidadElegida;
-                                    tarjeta.Saldo -= cantidadElegida * libro.Precio;
-                                    libro.Cantidad -= cantidadElegida;
-                                    Console.WriteLine($"Su total es: {totalCompra}");
+                                    else
+                                    {
+                                        Console.WriteLine("Datos de tarjeta invalidos...");
+                                    }
                                 }
                                 else
                                 {
-                                    Console.WriteLine("Saldo insuficiente...");
+                                    Console.WriteLine("Error cantidad invalida...");
                                 }
                             }
-                            else
-                            {
-                                Console.WriteLine("Datos de tarjeta invalidos...");
-                            }
+                        }
+                        else if (codigoElegido == 00)
+                        {
+                            comprado = true;
                         }
                         else
                         {
-                            Console.WriteLine("Error cantidad invalida...");
+                            encontrado = false;
                         }
                     }
-                }
-                else if (codigoElegido == 00)
-                {
-                    comprado = true;
-                }
-                else
-                {
-                    encontrado = false;
-                }
-            }
-            if (!encontrado)
-            {
-                Console.WriteLine("El libro elegido no existe...");
-            }
-        }
-    }
-    }
-}
+                    if (!encontrado)
+                    {
+                        Console.WriteLine("El libro elegido no existe...");
+                    }
+
+                }//While comprando
+            }//ComprarLibros()
+        }//Main
+    }//Program
+}//Namespace
